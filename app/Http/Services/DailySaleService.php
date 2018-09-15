f36d17b4ed55827a2688a325704d90c5e9adf745
Drew Payment <drew@verostack.io>
2018-09-14 23:59:47 -0400

    bug fix to handling sales tracker changes

--- app/Http/Services/DailySaleService.php
+++ app/Http/Services/DailySaleService.php
@@ -168,7 +168,7 @@ class DailySaleService {
 		}
 
 		return $result->setData(
-			DailySale::with('remarks')
+			DailySale::with('remarks.user')
 			         ->byDailySale($sale->dailySaleId)
 			         ->first());
 	}
