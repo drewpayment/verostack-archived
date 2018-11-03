18e8c75828616da801a1e0affc88938c2c7060fe
Drew Payment <drew@verostack.io>
2018-11-03 16:12:51 -0400

    build erros

--- public/dist/index.php
+++ public/dist/index.php
@@ -24,7 +24,7 @@
         }
     </script>
 
-<link rel="stylesheet" href="dist/styles.css"></head>
+<link rel="stylesheet" href="dist/styles.5ee54ca51282db99fbab.css"></head>
 <body>
 
     <app-root>
@@ -50,5 +50,5 @@
 
 
 
-<script type="text/javascript" src="dist/runtime.js"></script><script type="text/javascript" src="dist/polyfills.js"></script><script type="text/javascript" src="dist/vend.lib.js"></script><script type="text/javascript" src="dist/vendor.js"></script><script type="text/javascript" src="dist/main.js"></script></body>
+<script type="text/javascript" src="dist/runtime.90050927e0c994a937e4.js"></script><script type="text/javascript" src="dist/polyfills.6f97a6de716e35ce02b2.js"></script><script type="text/javascript" src="dist/vend.lib.917d066848c5f8907f85.js"></script><script type="text/javascript" src="dist/main.0190b64686624d91a9b6.js"></script></body>
 </html>
